befdfd5300c91fb0cf7bfc67d0290a69a405d3e1
seanstaka <114537359+seanstaka@users.noreply.github.com>
2022-09-27 13:03:21 -1000

    Add files via upload

---
+++ 03_Interactive_storybook/app.js
@@ -0,0 +1,7 @@
+// STRECH GOAl: JAVASCRIPT (will learn next week!)
+
+var button1 = document.getElementById('button1');
+
+button1.onclick = function() {
+  dialogue1.innerHTML = "Once upon a time...";
+}
